c45ed5e298036c0ded9c059c4964d5e2a07a9313
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2024-01-16 20:45:50 -0800

    Install gopls during setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ yes | goenv install --skip-existing
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2
 
 go install mvdan.cc/gofumpt@latest
+
+go install golang.org/x/tools/gopls@latest
