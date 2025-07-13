4978afb25199dcc5be8faab51f036cf5e256f864
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2025-07-13 12:02:12 -0600

    Fix: Latest golangci-lint

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 mise settings add idiomatic_version_file_enable_tools go
 mise install
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.2.2
 
 go install mvdan.cc/gofumpt@latest
 
