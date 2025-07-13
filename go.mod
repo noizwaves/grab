2e522b7671640cf7090d9640ef6388825107745a
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2025-07-13 11:50:11 -0600

    Upgrade to Go 1.24.5

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/noizwaves/grab
 
-go 1.21.6
+go 1.24.5
 
 require (
 	github.com/spf13/cobra v1.8.0
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+mise settings add idiomatic_version_file_enable_tools go
 mise install
 
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.55.2
